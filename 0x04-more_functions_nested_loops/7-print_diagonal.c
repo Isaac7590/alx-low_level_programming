fbc8930e773d9ec7e6e6350d74cc32627dd23f1d
Isaac7590 <odutuisaac7@gmail.com>
2022-07-08 18:16:00 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * vhggkry
+ */
+void print_diagonal(int n)
+{
+
+int i;
+int b;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (b = 0; b < i; b++)
+{
+_putchar (' ');
+}
+_putchar (92);
+_putchar ('\n');
+
+}
+
+}
+
